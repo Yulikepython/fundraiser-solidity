8525559dd29564bb12642894b96a778b757313d8
Yu <admin@itc.tokyo>
2022-02-01 15:36:29 +0900

    adding fundraiser app till new fundraiser

--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -7,7 +7,7 @@ contract("Fundraiser", accounts => {
     const imageURL = "https://placekitten.com/600/350";
     const description = "Beneficiary description";
     const beneficiary = accounts[1];
-    const custodian = accounts[0];
+    const owner = accounts[0];
 
     describe("initialization", ()=>{
         beforeEach (async () => {
@@ -17,7 +17,7 @@ contract("Fundraiser", accounts => {
                 imageURL,
                 description,
                 beneficiary,
-                custodian
+                owner
             )
         });
         it("gets the beneficiary name", async () => {
@@ -40,9 +40,31 @@ contract("Fundraiser", accounts => {
             const actual = await fundraiser.beneficiary();
             assert.equal(actual, beneficiary, "beneficiary address should match");
         });
-        it("gets the custodian", async () => {
-            const actual = await fundraiser.custodian();
-            assert.equal(actual, custodian, "custodian should match");
+        it("gets the owner", async () => {
+            const actual = await fundraiser.owner();
+            assert.equal(actual, owner, "bios should match");
+        });
+    });
+
+    describe("setBeneficiary", ()=>{
+        const newBeneficiary = accounts[2];
+        
+        it("updated beneficiary when called by owner account", async ()=>{
+            await fundraiser.setBeneficiary(newBeneficiary, {from: owner});
+            const actualBeneficiary = await fundraiser.beneficiary();
+            assert.equal(actualBeneficiary, newBeneficiary, "beneficiaries should match!");
+        });
+        it("throws an error when called from a non-owner account", async ()=>{
+            try {
+                await fundraiser.setBeneficiary(newBeneficiary, {from: accounts[3]});
+                assert.fail("withdraw was not restricted to owners")
+            } catch( err) {
+                const expectedError = "Ownable: caller is not the owner"
+                const actualError = err.reason;
+                // console.log("actual error: ", actualError)
+                // console.log("err: ", err)
+                assert.equal(actualError, expectedError, "should not be permitted")
+            }
         });
     });
 });
