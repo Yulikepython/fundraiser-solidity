7a51b63b0496ecb6e8cd0712c9d10298de126bea
Yu <admin@itc.tokyo>
2022-02-01 13:11:07 +0900

    up to 106

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -6,17 +6,22 @@ contract Fundraiser {
     string public url;
     string public imageURL;
     string public description;
+    address payable public beneficiary; //受取人
+    address public custodian;
 
     constructor (
         string memory _name,
         string memory _url,
         string memory _imageURL,
-        string memory _description
+        string memory _description,
+        address payable _beneficiary,
+        address _custodian
         ) public {
         name = _name;
         url = _url;
         imageURL = _imageURL;
         description = _description;
-
+        beneficiary = _beneficiary;
+        custodian = _custodian;
     }
 }
